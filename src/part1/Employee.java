163d973fdff0cee63b32a1f0aa1e9976647fac39
Marcello Muylaert <87388600+MarcelloMuy@users.noreply.github.com>
2024-02-26 21:00:25 +0000

    Create Employee Class and two constructors

---
+++ src/part1/Employee.java
@@ -0,0 +1,30 @@
+package part1;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Marce
+ */
+public class Employee {
+    
+    private String name;
+    private String email;
+    private int empNum;
+    
+    private static int nextEmpNum = 1;
+    
+    public Employee() {
+        this.name = "Employee Name";
+        this.email = "employee@email.com";
+    }
+    
+    public Employee(String name, String email) {
+        this.name = name;
+        this.email = email;
+        this.empNum = nextEmpNum++;
+    }    
+}
