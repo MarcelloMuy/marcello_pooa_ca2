2f4c65c52889f2c332b36e2f2737ed174e36d5f5
Marcello Muylaert <87388600+MarcelloMuy@users.noreply.github.com>
2024-02-27 20:40:35 +0000

    Create EmployeeTest Class and 3 Employee objects

---
+++ src/part1/EmployeeTest.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package part1;
+
+/**
+ *
+ * @author Marce
+ */
+// EmployeeTest Class creates 3 Employee objects,
+// stores objects into an array and prints out the employee number
+public class EmployeeTest {
+//    Create 3 employee objects
+    public static void main(String[] args) {
+        Employee employeeJoe = new Employee("Joe Bloggs", "jb@gmail.com");
+        Employee employeeAnn = new Employee("Ann Banana", "ab@gmail.com");
+        Employee employeeTom = new Employee("Tom Thumb", "tt@gmail.com");
+    }
+}
