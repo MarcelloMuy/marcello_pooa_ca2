e3df9f1bf9215e996a090e9432a74cd0c3668170
Marcello Muylaert <87388600+MarcelloMuy@users.noreply.github.com>
2024-02-27 22:07:10 +0000

    Iterates projectGroup array and display object
    Search for employees with "empNum" above value of variable "m" and display their name

--- src/part1/EmployeeTest.java
+++ src/part1/EmployeeTest.java
@@ -11,11 +11,15 @@ package part1;
 // EmployeeTest Class creates 3 Employee objects,
 // stores objects into an array and prints out the employee number
 public class EmployeeTest {
-//    Create 3 employee objects
+//m variable created for testing
+    public static int m = 1;
+    
+//    Create 3 employee objects    
     public static void main(String[] args) {
         Employee employeeJoe = new Employee("Joe Bloggs", "jb@gmail.com");
         Employee employeeAnn = new Employee("Ann Banana", "ab@gmail.com");
         Employee employeeTom = new Employee("Tom Thumb", "tt@gmail.com");
+        
 
 //    Array to store 3 employee objects
         Employee[] projectGroup = new Employee[3];  
@@ -27,5 +31,13 @@ public class EmployeeTest {
         
 //    Prints the value of the next employee number
         System.out.println(Employee.nextEmpNum);
+        
+//     Iterates through the projectGroup array and
+//     display employees with "empNum" above the value of variable "m"
+        for (Employee employee : projectGroup) {
+            if (employee.empNum > m) {
+                System.out.println("Name: " + employee.getName());
+            }
+        }
     }
 }
