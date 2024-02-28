7892d1043ba8b6fbbfba535be0a657b2c0acb05c
Marcello Muylaert <87388600+MarcelloMuy@users.noreply.github.com>
2024-02-28 18:55:36 +0000

    Create a Company Class with a field for company name and an ArrayList for employees

---
+++ src/part1/Company.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package part1;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Marce
+ */
+public class Company {
+    
+//  Instance field for company name    
+    public String companyName;
+    
+//  Create an arrayList to store Employee objects 
+    ArrayList<Employee> staff = new ArrayList<>();
+
+}
