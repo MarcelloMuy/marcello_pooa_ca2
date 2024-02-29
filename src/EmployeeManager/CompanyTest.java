d42a5266f6cc3c04488f5464c77bc1595b8e8dc7
Marcello Muylaert <87388600+MarcelloMuy@users.noreply.github.com>
2024-02-29 20:59:31 +0000

    Test Company class methods

---
+++ src/EmployeeManager/CompanyTest.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package EmployeeManager;
+
+/**
+ *
+ * @author Marce
+ */
+public class CompanyTest {
+    
+    public static void main(String[] args) {
+        
+//        Test company New company
+        Company company = new Company("Test Company");
+        
+//        Test add new staff
+        company.addnewStaff(new Employee("Bob Marley", "bm@gmail.com"));
+        
+//        Test get staff number
+        System.out.println("Number of employees: " + company.getStaffNumber());
+    }
+    
+}
