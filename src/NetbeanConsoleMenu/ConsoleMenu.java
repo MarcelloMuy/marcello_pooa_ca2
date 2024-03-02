6fe3606882f5893f2a8a49c31794e51c33b440a0
Marcello Muylaert <87388600+MarcelloMuy@users.noreply.github.com>
2024-03-02 10:58:37 +0000

    Add comments and rename package and class name

--- src/NetbeanConsoleMenu/ConsoleMenu.java
+++ src/NetbeanConsoleMenu/ConsoleMenu.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package marcello_pooa_ca2;
+package NetbeanConsoleMenu;
 
 import EmployeeManager.*;
 import java.util.Scanner;
@@ -11,29 +11,34 @@ import java.util.Scanner;
  *
  * @author Marce
  */
-public class Marcello_pooa_ca2 {
+public class ConsoleMenu {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
+//      Create company with deafult values
+        Company company = new Company();
+//      Calls the login method
         Manager managerlogin = login();
 
     }
-
+//    Method for manager login
     private static Manager login() {
+//       Display message asking for Username
         Scanner scanner = new Scanner(System.in);
         System.out.println("Username: ");
         String username = scanner.nextLine();
-
+//      Display message asking for Password
         System.out.println("Password: ");
         String password = scanner.nextLine();
-
+//        Check if creditials are matching
         if ("Gnomeo".equals(username) && "smurf".equals(password)) {
+//            Manager are logged in
             System.out.println("You are logged in!");
             return new Manager("name", "email", username, password);
         } else {
+//            Wrong or invalid credentials
             System.out.println("Invalid credentials. Exiting...");
             return null;
         }
