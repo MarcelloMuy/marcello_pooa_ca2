c0179c7b19c94ac12449c08750e2187f8c28a3e5
Marcello Muylaert <87388600+MarcelloMuy@users.noreply.github.com>
2024-03-02 11:06:56 +0000

    Add new employees to staff list

--- src/NetbeanConsoleMenu/ConsoleMenu.java
+++ src/NetbeanConsoleMenu/ConsoleMenu.java
@@ -19,12 +19,23 @@ public class ConsoleMenu {
     public static void main(String[] args) {
 //      Create company with deafult values
         Company company = new Company();
+
+//        Add employees to staff list
+        company.addnewStaff(new Employee("Joe Bloggs", "jb@gmail.com"));
+        company.addnewStaff(new Employee("Ann Banana", "ab@gmail.com"));
+        company.addnewStaff(new Employee("Tom Thumb", "tt@gmail.com"));
+        company.addnewStaff(new Employee("Bob Marley", "bm@gmail.com"));
+        company.addnewStaff(new Employee("Mark Cuban", "mc@gmail.com"));
+        company.addnewStaff(new Employee("Elon Musk", "em@gmail.com"));
+
 //      Calls the login method
         Manager managerlogin = login();
 
     }
 //    Method for manager login
+
     private static Manager login() {
+
 //       Display message asking for Username
         Scanner scanner = new Scanner(System.in);
         System.out.println("Username: ");
